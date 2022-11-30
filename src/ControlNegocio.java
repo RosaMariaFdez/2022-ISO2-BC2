65aa169b94d27e5e7d7428b9de6070bcbb36672f
Rosa María <115668275+RosaMariaFdez@users.noreply.github.com>
2022-11-30 21:17:53 +0100

    Añadida implementación

---
+++ src/ControlNegocio.java
@@ -0,0 +1,8 @@
+public class ControlNegocio {
+
+	public void addNegocio() {
+		// TODO - implement ControlNegocio.addNegocio
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Peregrino.java
@@ -0,0 +1,48 @@
+public class Peregrino extends Usuarios {
+
+	/**
+	 * 
+	 * @param pi
+	 */
+	public void addPuntoInteres(PuntoInteres pi) {
+		// TODO - implement Peregrino.addPuntoInteres
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ru
+	 */
+	public void verRuta(Ruta ru) {
+		// TODO - implement Peregrino.verRuta
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ru
+	 */
+	public void planificarRuta(Ruta ru) {
+		// TODO - implement Peregrino.planificarRuta
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param pi
+	 */
+	public void consultarPuntoInteres(PuntoInteres pi) {
+		// TODO - implement Peregrino.consultarPuntoInteres
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ru
+	 */
+	public void crearBitacora(Ruta ru) {
+		// TODO - implement Peregrino.crearBitacora
+		throw new UnsupportedOperationException();
+	}
+
+}
