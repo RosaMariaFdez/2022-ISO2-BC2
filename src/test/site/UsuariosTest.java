1ec6cba59d429e33414c9248f21e63ccffbf7048
Bersaglieri412 <92685068+Bersaglieri412@users.noreply.github.com>
2022-12-10 20:45:57 +0100

    Testing realizado

---
+++ src/test/site/UsuariosTest.java
@@ -0,0 +1,20 @@
+public class Usuarios {
+
+	private int idUsuario;
+	private String nombre;
+	private String pwd;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(int nombre) {
+		// TODO - implement Usuarios.setNombre
+		throw new UnsupportedOperationException();
+	}
+
+}
