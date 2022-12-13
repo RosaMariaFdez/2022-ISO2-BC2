834e1318c0a828e2e184a03749766bd9249a846a
Bersaglieri412 <92685068+Bersaglieri412@users.noreply.github.com>
2022-12-13 18:37:15 +0100

    Listo release

---
+++ General V.1.4.0/src/main/java/BC02/Negocioss.java
@@ -0,0 +1,22 @@
+public class Negocioss {
+	private int _idNegocio;
+	private String _nombreNegocio;
+	private double _valorar;
+	public Valorar _unnamed_Valorar_;
+
+	public String getNombreNegocio() {
+		return this._nombreNegocio;
+	}
+
+	public void setNombreNegocio(Object aNombreNegocio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public double getValoracion() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setValoracion(double aVal) {
+		throw new UnsupportedOperationException();
+	}
+}
