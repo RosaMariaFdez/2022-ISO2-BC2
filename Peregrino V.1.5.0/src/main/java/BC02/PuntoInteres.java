715aed30b91452ae7964cc75e8b377bac9e9c2e0
Bersaglieri412 <92685068+Bersaglieri412@users.noreply.github.com>
2022-12-19 18:47:17 +0100

    Formato release final

---
+++ Peregrino V.1.5.0/src/main/java/BC02/PuntoInteres.java
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+public class PuntoInteres {
+
+	private int idPuntoInteres;
+	private String nombre;
+	private String coordenadas;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getCoords() {
+		// TODO - implement PuntoInteres.getCoords
+		throw new UnsupportedOperationException();
+	}
+
+=======
+public class PuntoInteres {
+
+	private int idPuntoInteres;
+	private String nombre;
+	private String coordenadas;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getCoords() {
+		// TODO - implement PuntoInteres.getCoords
+		throw new UnsupportedOperationException();
+	}
+
+>>>>>>> It2-Desarrollo
+}
